3bc0443949b3165990464ea7ba4f2bf107a4fe7c
Chris Sjoblom <sjoblom@uoregon.edu>
2013-10-02 10:33:54 -0700

    added settings file example

---
+++ default_settings_example.py
@@ -0,0 +1,9 @@
+#RSVP Configuration
+DEBUG = True
+
+"""Secret key is a randomly generated series of numbers/letters/special characters, required for form access."""
+SECRET_KEY = ''
+
+"""Username and password correspond to admin credentials."""
+USERNAME = ''
+PASSWORD = ''
